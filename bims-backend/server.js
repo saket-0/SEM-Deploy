4e09f2438410d4d3ce6f54258585dfe5783241cf
Saket <kumar.saketpal@gmail.com>
2025-11-08 02:26:46 +0530

    Check 6

--- bims-backend/server.js
+++ bims-backend/server.js
@@ -79,8 +79,8 @@ app.use(session({
     cookie: {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'none',
         path: '/',
         domain: undefined
     },
